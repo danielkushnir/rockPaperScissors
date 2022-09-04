f5547fde52b3c94bb53210d6b790d6d00cff2e40
daniel <dani2msn@gmail.com>
2022-09-04 10:52:05 -0700

    Add playRound func

--- script.js
+++ script.js
@@ -8,3 +8,31 @@ function getComputerChoice() {
         return "scissors";
     }
 }
+
+function playRound(playerSelection, compuerSelection) {
+    if (playerSelection.toLowerCase() === 'rock') {
+        if (compuerSelection === 'rock') {
+            return "It's a tie!";
+        } else if (compuerSelection === 'paper') {
+            return "You lose! Paper beats rock.";
+        } else {
+            return "You win! Rock beats scissors.";
+        }
+    } else if (playerSelection.toLowerCase() === 'paper') {
+        if (compuerSelection === 'rock') {
+            return "You win! Paper beats rock.";
+        } else if (compuerSelection === 'paper') {
+            return "It's a tie!";
+        } else {
+            return "You lose! Scissors beats paper";
+        }
+    } else {
+        if (compuerSelection === 'rock') {
+            return "You lose! Rock beats scissors";
+        } else if (compuerSelection === 'paper') {
+            return "You win! Scissors beats paper.";
+        } else {
+            return "It's a tie!";
+        }
+    }
+}
