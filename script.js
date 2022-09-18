65f26679e09f192a2a5145fb1bfe0acba8ef8866
daniel <dani2msn@gmail.com>
2022-09-17 21:41:10 -0700

    Add resetGame function

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-let playerScore = 0;
 const playerScorePara = document.querySelector("#playerScore");
-let computerScore = 0;
 const computerScorePara = document.querySelector("#computerScore");
-
+let playerScore;
+let computerScore;
+resetGame();
 
 function getComputerChoice() {
     const computerOptions = ['rock', 'paper', 'scissors'];
     return computerOptions[Math.floor(Math.random() * 3)];
+}
+
+function resetGame() {
+    playerScore = 0;
+    playerScorePara.textContent = `Player Score: ${playerScore}`;
+    computerScore = 0;
+    computerScorePara.textContent = `Computer Score: ${computerScore}`;
 }
 
 function tieRound() {
@@ -32,6 +39,7 @@ function computerWinsRound() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    if (playerScore === 5 || computerScore === 5) resetGame();
     if (playerSelection.toLowerCase() === computerSelection) {
         tieRound();
     } else if ((playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') ||
