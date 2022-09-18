eddeb60764a24dce3d0b992a1fca58a6b57b9dd2
daniel <dani2msn@gmail.com>
2022-09-17 21:26:43 -0700

    Add functions for each round opt

--- script.js
+++ script.js
@@ -1,45 +1,52 @@
+let playerScore = 0;
+const playerScorePara = document.querySelector("#playerScore");
+let computerScore = 0;
+const computerScorePara = document.querySelector("#computerScore");
+
+
 function getComputerChoice() {
-    let computerChoiceNum = Math.floor(Math.random() * 3) + 1;
-    if (computerChoiceNum === 1) {
-        return "rock";
-    } else if (computerChoiceNum === 2) {
-        return "paper";
-    } else {
-        return "scissors";
+    const computerOptions = ['rock', 'paper', 'scissors'];
+    return computerOptions[Math.floor(Math.random() * 3)];
+}
+
+function tieRound() {
+    results.textContent = "It's a tie!";
+}
+
+function playerWinsRound() {
+    results.textContent = "You win this round!";
+    playerScore++;
+    playerScorePara.textContent = `Player Score: ${playerScore}`;
+    if (playerScore === 5) {
+        results.textContent = "Game over! You win the game!";
+    }
+}
+
+function computerWinsRound() {
+    results.textContent = "You lose this round!";
+    computerScore++;
+    computerScorePara.textContent = `Computer Score: ${computerScore}`;
+    if (computerScore === 5) {
+        results.textContent = "Game over! You lose the game!";
     }
 }
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() === computerSelection) {
-        console.log("It's a tie!");
-        return "It's a tie!";
+        tieRound();
+    } else if ((playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock') ||
+    (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper')) {
+        playerWinsRound();
+    } else {
+        computerWinsRound();
+       
+        
     }
-    if (playerSelection.toLowerCase() === 'rock') {
-        if (computerSelection === 'paper') {
-            console.log("You lose this round!");
-            return "You lose this round!";
-        } else {
-            console.log("You win this round!");
-            return "You win this round!";
-        }
-    } else if (playerSelection.toLowerCase() === 'paper') {
-        if (computerSelection === 'rock') {
-            console.log("You win this round!");
-            return "You win this round!";
-        } else {
-            console.log("You lose this round!");
-            return "You lose this round!";
-        }
-    } else if (playerSelection.toLowerCase() === 'scissors') {
-        if (computerSelection === 'rock') {
-            console.log("You lose this round!");
-            return "You lose this round!";
-        } else {
-            console.log("You win this round!");
-            return "You win this round!";
-        }
-    }
+            
 }
+
+const results = document.querySelector("#results");
 
 const playerBtns = document.querySelectorAll("#playerBtns button");
 playerBtns.forEach(btn => btn.addEventListener('click', () => playRound(btn.textContent, getComputerChoice())));
